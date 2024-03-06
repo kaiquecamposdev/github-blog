6aed1a18b1a1200710e2b912f94679c1209e87f1
Kaique <kaiquedecampos2@gmail.com>
2024-03-06 19:38:45 -0300

    feat: update tailwind.config.js with new styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,8 +9,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      maxWidth: {
+        'main-content': '864px',
+      },
       backgroundImage: {
-        logo: 'url("/public/logo.png")',
+        logo: 'url("/logo.png")',
+      },
+      textColor: {
+        'base-blue': '#3294F8',
+        'base-title': '#E7EDF4',
+        'base-subtitle': '#C4D4E3',
+        'base-text': '#AFC2D4',
+        'base-span': '#7B96B2',
+        'base-label': '#3A536B',
+        'base-border': '#1C2F41',
+        'base-post': ' #112131',
+      },
+      backgroundColor: {
+        base: '#071422',
+        'base-profile': '#0B1B2B',
       },
     },
   },
