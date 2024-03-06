9af226f8d9d70ebe7412c9509e6f7aeb5c2bec36
kaiquedecampos2@gmail.com <kaiquedecampos2@gmail.com>
2024-03-06 01:17:42 -0300

    raw: add logo background image to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        logo: 'url("/public/logo.png")',
+      },
+    },
   },
   plugins: [],
 }
