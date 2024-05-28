ae37a7d4268392b72a7e1294171b5709ef0c967f
Kaique <kaiquedecampos2@gmail.com>
2024-05-28 14:55:31 -0300

    fix: fixed a bug that returned the initial state as undefined

--- src/components/repos-posts.tsx
+++ src/components/repos-posts.tsx
@@ -22,40 +22,39 @@ export function ReposPosts() {
     )
   })
 
-  const paginatedRepos = CreatePagination(formattedReposPost)
+  const paginatedRepos =
+    CreatePagination(formattedReposPost)[indexThePage] || formattedReposPost
 
   return (
     <ul className="grid grid-cols-2 gap-8">
-      {paginatedRepos[indexThePage].map(
-        ({ id, name, created_at, description }) => {
-          return (
-            <li
-              key={id}
-              className="h-[260px] w-full max-w-[416px] cursor-pointer rounded-[10px] bg-base-post p-8 transition-shadow hover:ring-2 hover:ring-slate-500"
+      {paginatedRepos.map(({ id, name, created_at, description }) => {
+        return (
+          <li
+            key={id}
+            className="h-[260px] w-full max-w-[416px] cursor-pointer rounded-[10px] bg-base-post p-8 transition-shadow hover:ring-2 hover:ring-slate-500"
+          >
+            <Link
+              className="flex h-full flex-col gap-5"
+              href={`post/${id}` || '#'}
             >
-              <Link
-                className="flex h-full flex-col gap-5"
-                href={`post/${id}` || '#'}
-              >
-                <span className="flex gap-4">
-                  <h1 className="w-full text-xl font-bold text-base-title">
-                    {name}
-                  </h1>
-                  <time className="text-nowrap text-sm text-base-span">
-                    {formatDistance(created_at, new Date(), {
-                      addSuffix: true,
-                      locale: ptBR,
-                    })}
-                  </time>
-                </span>
-                <Markdown className="overflow-hidden text-base-text">
-                  {description}
-                </Markdown>
-              </Link>
-            </li>
-          )
-        },
-      )}
+              <span className="flex gap-4">
+                <h1 className="w-full text-xl font-bold text-base-title">
+                  {name}
+                </h1>
+                <time className="text-nowrap text-sm text-base-span">
+                  {formatDistance(created_at, new Date(), {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </time>
+              </span>
+              <Markdown className="overflow-hidden text-base-text">
+                {description}
+              </Markdown>
+            </Link>
+          </li>
+        )
+      })}
     </ul>
   )
 }
